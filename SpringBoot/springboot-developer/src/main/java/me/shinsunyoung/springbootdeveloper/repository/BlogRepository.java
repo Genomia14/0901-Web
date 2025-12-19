bdcddbe903b4059444441b25bc13b94612bac9f8
DESKTOP-6MPHVM6\it <zeta2011@naver.com>
2025-12-19 16:33:11 +0900

    2025-12-19 Spring-boot

--- SpringBoot/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/repository/BlogRepository.java
+++ SpringBoot/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/repository/BlogRepository.java
@@ -4,6 +4,9 @@ import me.shinsunyoung.springbootdeveloper.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BlogRepository extends JpaRepository<Article, Long> {
+    List<Article> findByTitle(String title);
 }
