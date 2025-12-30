adbda3e44b4da1f68cce28963bc43e3bb9d93c76
DESKTOP-6MPHVM6\it <zeta2011@naver.com>
2025-12-30 16:36:48 +0900

    2025-12-30 Spring-boot

--- SpringBoot/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogViewController.java
+++ SpringBoot/springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/controller/BlogViewController.java
@@ -32,14 +32,18 @@ public class BlogViewController {
     }
 
     @GetMapping("/articles/{id}")
-    public String findArticle(@PathVariable("id") long id, Model model) {
+    public String findArticle(@PathVariable("id") long id,
+                              PageRequestDTO pageRequestDTO,
+                              Model model) {
         Article article = blogService.findById(id);
         model.addAttribute("article", new ArticleViewResponse(article));
         return "article";
     }
 
     @GetMapping("/new-article")
-    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+    public String newArticle(@RequestParam(required = false) Long id,
+                             PageRequestDTO pageRequestDTO,
+                             Model model) {
         if (id == null) {
             // 글 추가
             model.addAttribute("article", new ArticleViewResponse());
